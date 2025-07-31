f77ae73f3fbca21c83641debc353168bd0ad63e1
Mahidur Rahman <mahidur952697@gmail.com>
2025-07-31 23:11:24 +0600

    first commit

---
+++ java.js
@@ -0,0 +1,11 @@
+// console.log("Hello, World!");
+var weight = 70; // weight in kg
+var price = 100; // price in dollars
+console.log(weight)
+var name =" Mahidur";
+console.log(name);
+// vvollean
+var isActive = true; // boolean value
+console.log(isActive);
+var isnotActive = false; // boolean value
+console.log(isnotActive);
